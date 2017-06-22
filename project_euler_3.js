270f678683db5e3a1cdad832bf0c65d3aa43003c
Liz Parody <lizparody@gmail.com>
2017-06-22 18:17:59 -0400

    added the foor loop answer

--- project_euler_3.js
+++ project_euler_3.js
@@ -1,3 +1,17 @@
+function largestPrimeFactor(num){
+	for(i = 2; i < num; i++){
+		if(num % i === 0){
+			num /= i;
+		}
+	}
+	return i;
+}
+
+var num = 600851475143;
+largestPrimeFactor(num);
+
+//With a while loop
+
 function largestPrimeFactor(number) {
   var i = 2;
   while (i <= number){
