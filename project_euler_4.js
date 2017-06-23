4bae1535689ebf54a409e6d91fa0953ab308ca90
Liz Parody <lizparody@gmail.com>
2017-06-23 12:16:43 -0400

    Added the solution of Project Euler #4

--- project_euler_4.js
+++ project_euler_4.js
@@ -1,11 +1,14 @@
-function palindrome(word) {
-   if (word == word.split("").reverse().join("")){
-    return true;
-   } else {
-    return false;
-   }
+function palindromic() {
+  var max = 0;
+  for(var i = 999; i > 99; i--) {
+    for(var j = 999; j > 99; j--) {
+      var n = i * j;
+      var s = String(n);
+      if (s === s.split("").reverse().join("") && n > max) {
+        max = n;
+      }
+    }
+  }
+  return max;
 }
-
-console.log(palindrome("noon"));
-
-
+console.log(palindromic());
